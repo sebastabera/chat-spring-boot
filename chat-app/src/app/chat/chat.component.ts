d6d7817a3cede1321e851eee975c65b14f39ea47
Luis Sebastian Tabera Quintero <lstabera@personalsoft.com.co>
2019-10-15 16:14:17 -0500

    chat enviar y recibir mensajes y conexion de usuarios

--- chat-app/src/app/chat/chat.component.ts
+++ chat-app/src/app/chat/chat.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Client } from '@stomp/stompjs';
 import * as SockJS from 'sockjs-client';
+import { Mensaje } from './models/mensaje';
 
 @Component({
   selector: 'app-chat',
@@ -10,6 +11,9 @@ import * as SockJS from 'sockjs-client';
 export class ChatComponent implements OnInit {
 
   private client: Client;
+  conectado: boolean = false;
+  mensaje: Mensaje = new Mensaje();
+  mensajes:Mensaje[] = [];
   constructor() { }
 
   ngOnInit() {
@@ -28,10 +32,50 @@ export class ChatComponent implements OnInit {
       //Las tareas de conexion
       //el objeto frame tiene toda la informacion de nuestra conexion con el broker
       console.log('Conectados: ' + this.client.connected + ' : ' + frame);
+      this.conectado = true;
+
+      this.client.subscribe('/chat/mensaje', e => {
+        //Se convierte el String a un objeto de tipo mensaje
+        let mensaje:Mensaje = JSON.parse(e.body) as Mensaje;
+        mensaje.fecha = new Date(mensaje.fecha);
+        this.mensajes.push(mensaje);
+        console.log(mensaje);
+      });
+
+      this.client.publish({
+        destination: '/app/mensaje',
+        body: JSON.stringify(this.mensaje)
+      });
     }
 
-    //inicializar la conexion 
+    this.mensaje.tipo = 'NUEVO_USUARIO';
+    this.client.onDisconnect = (frame) => {
+      console.log('Desconectados: ' + this.client.connected + ' : ' + frame);
+      this.conectado = false;
+    }
+  }
+
+  /**
+   * Metodo para iniciar la conexion
+   */
+  conectar():void {
     this.client.activate();
+  }
+
+  /**
+   * metodo para finalizar la conexion
+   */
+  desconectar():void {
+    this.client.deactivate();
+  }
+
+  enviarMensaje():void {
+    this.mensaje.tipo = 'MENSAJE';
+    this.client.publish({
+      destination: '/app/mensaje',
+      body: JSON.stringify(this.mensaje)
+    });
+    this.mensaje.texto = '';
   }
 
 }
--- spring-boot-backend-chat/src/main/java/com/estudio/springboot/backend/chat/models/documents/Mensaje.java
+++ spring-boot-backend-chat/src/main/java/com/estudio/springboot/backend/chat/models/documents/Mensaje.java
@@ -7,6 +7,26 @@ public class Mensaje implements Serializable {
 	private String texto;
 
 	private Long fecha;
+	
+	private String username;
+	
+	private String tipo;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
 
 	public String getTexto() {
 		return texto;
